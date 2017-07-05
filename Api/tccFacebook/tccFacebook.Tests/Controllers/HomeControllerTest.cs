baf29629270a96f5d3960f269217feadd469f516
Jabel <jabel.fontoura@cwi.com.br>
2017-07-05 18:48:19 -0400

    feat(Esqueleto): adicionar esqueleto do projeto

---
+++ Api/tccFacebook/tccFacebook.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tccFacebook;
+using tccFacebook.Controllers;
+
+namespace tccFacebook.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
