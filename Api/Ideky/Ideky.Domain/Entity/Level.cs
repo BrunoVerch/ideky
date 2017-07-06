e54b58b6ab2db8b29e372433ad1f4d33f561e24e
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-06 18:39:58 -0300

    feat(Level): implementação de entidade para níveis

---
+++ Api/Ideky/Ideky.Domain/Entity/Level.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ideky.Domain.Entity
+{
+    public class Level : BasicEntity
+    {
+        public int Id { get; private set; }
+        public int LevelNumber { get; private set; }
+        public int PictureAmount { get; private set; }
+        public int Duration { get; private set; }
+        public int Multiplier { get; private set; }
+
+        public List<string> Messages { get; private set; }
+
+        protected Level() { }
+
+        public Level(int levelNumber, int pictureAmount, int duration, int multiplier)
+        {
+            Id = 0;
+            LevelNumber = levelNumber;
+            PictureAmount = pictureAmount;
+            Duration = duration;
+            Multiplier = multiplier;
+        }
+
+        public void UpdateLevelDifficult(int pictureAmount, int duration, int multiplier)
+        {
+            PictureAmount = pictureAmount;
+            Duration = duration;
+            Multiplier = multiplier;
+        }
+
+        public bool Validate()
+        {
+            if (LevelNumber < 0)
+            {
+                Messages.Add("Invalid level number!");
+            }
+            if (PictureAmount < 0)
+            {
+                Messages.Add("Invalid picture amount!");
+            }
+            if(Duration < 0)
+            {
+                Messages.Add("Invalid duration time!");
+            }
+            return Messages.Count == 0;
+        }
+    }
+}
