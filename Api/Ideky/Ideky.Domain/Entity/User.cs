17298eeba17a97f6bff8d342de859d35f4219d4f
Jabel <jabel.fontoura@cwi.com.br>
2017-07-05 21:37:31 -0400

    feat(entity): adicionar user

---
+++ Api/Ideky/Ideky.Domain/Entity/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideky.Domain.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+    }
+}
