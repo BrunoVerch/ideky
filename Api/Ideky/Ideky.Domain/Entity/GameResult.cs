13bb16aded3e5e9843aa62bdb4c267cb99108171
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-06 18:39:37 -0300

    feat(GameResult): implementação da entidade GameResult

---
+++ Api/Ideky/Ideky.Domain/Entity/GameResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ideky.Domain.Entity
+{
+    public class GameResult : BasicEntity
+    {
+        public int Id { get; private set; }
+        public User User { get; private set; }
+        public DateTime GameDate { get; private set; }
+        public List<string> Messages { get; private set; }
+
+        protected GameResult() { }
+
+        public GameResult(User user)
+        {
+            Id = 0;
+            User = user;
+            GameDate = DateTime.Now;
+            
+        }
+
+        public bool Validate()
+        {
+            if(User != null)
+            {
+                Messages.Add("Invalid User");
+            }
+            return Messages.Count == 0;
+        }
+    }
+}
