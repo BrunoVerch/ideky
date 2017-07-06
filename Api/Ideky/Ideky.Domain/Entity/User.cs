324cf1a74239d7940e65004925af1881b14a08ca
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-06 16:54:33 -0300

    feat(User): implementação da entidade usuário inicial

--- Api/Ideky/Ideky.Domain/Entity/User.cs
+++ Api/Ideky/Ideky.Domain/Entity/User.cs
@@ -8,6 +8,39 @@ namespace Ideky.Domain.Entity
 {
     public class User
     {
-        public int FacebookId { get; set; }
+        public int FacebookId { get; private set; }
+        public long Record { get; private set; }
+        public int Lifes { get; private set; }
+        public DateTime LastLogin { get; private set }
+
+        protected User() { }
+
+        public User(int facebookId)
+        {
+            FacebookId = facebookId;
+            Record = 0;
+            Lifes = 0;
+            LastLogin = DateTime.Now;
+        }
+
+        public void SetNewLogin()
+        {
+            LastLogin = DateTime.Now;
+        }
+     
+        public void SetNewRecord(int record)
+        {
+            Record = record;
+        }
+
+        public void AddLifes()
+        {
+            Lifes = Lifes + 3;
+        }
+
+        public void AddLifes(int lifes)
+        {
+            Lifes = Lifes + lifes;
+        }
     }
 }
