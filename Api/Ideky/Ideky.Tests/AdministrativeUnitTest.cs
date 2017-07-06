cf6c71fe622dbea1ae6b4edea707cc87d36612de
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-06 18:47:14 -0300

    feat(AdministrativeUnitTest): implementação de teste para o Administrativo

---
+++ Api/Ideky/Ideky.Tests/AdministrativeUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ideky.Domain.Entity;
+
+namespace Ideky.Tests
+{
+    [TestClass]
+    public class AdministrativeUnitTest
+    {
+        [TestMethod]
+        public void Create_Valid_Administrative_Entity()
+        {
+            //var admin = new Administrative();
+            //Assert.IsTrue(Administrative());
+            //Assert.IsFalse(Administrative.Mensagens.Count > 0);
+            
+        }
+    }
+}
