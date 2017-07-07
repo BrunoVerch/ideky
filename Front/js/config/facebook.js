2bc3f6f5fcbaab6486097c853e65c56ba33e082e
Jabel <jabel.fontoura@cwi.com.br>
2017-07-07 16:08:54 -0400

    feat(Facebook): adicionar configuração do facebook no angular

--- Front/js/config/facebook.js
+++ Front/js/config/facebook.js
@@ -1,27 +1,68 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '1392336224214575',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v2.9'
-  });
-  FB.AppEvents.logPageView();
+angular.module('app').run(function($rootScope, $window) {
+  console.log('teste');
 
-};
+  $rootScope.user = {};
 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    console.log('status',response);
-    FB.api('/me/invitable_friends?limit=99999', function(response) {
-        console.log(response);
+  $window.fbAsyncInit = function() {
+    // Executed when the SDK is loaded
+
+    console.log('SDK loaded')
+    FB.init({
+      appId: '1392336224214575',
+      status: true,
+      cookie: true,
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.9'
     });
-  });
-}
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+    //sAuth.watchAuthenticationStatusChange();
+  };
+  (function(d){
+    // load the Facebook javascript SDK
+    var js,
+    id = 'facebook-jssdk',
+    ref = d.getElementsByTagName('script')[0];
+
+    if (d.getElementById(id)) {
+      return;
+    }
+
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+
+    ref.parentNode.insertBefore(js, ref);
+
+  }(document));
+
+});
+
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId            : '1392336224214575',
+//     autoLogAppEvents : true,
+//     xfbml            : true,
+//     version          : 'v2.9'
+//   });
+//   FB.AppEvents.logPageView();
+
+// };
+
+// function checkLoginState() {
+//   FB.getLoginStatus(function(response) {
+//     console.log('status',response);
+//     FB.api('/me/invitable_friends?limit=99999', function(response) {
+//         console.log(response);
+//     });
+//   });
+// }
+
+// (function(d, s, id){
+//   var js, fjs = d.getElementsByTagName(s)[0];
+//   if (d.getElementById(id)) {return;}
+//   js = d.createElement(s); js.id = id;
+//   js.src = "//connect.facebook.net/en_US/sdk.js";
+//   fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
