0a5630aea0836c90f96d3fc7e0057621f13c562d
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-07 15:21:35 -0300

    fix(Score): implementação do score e mudança do DbSet de User para users

--- Api/Ideky/Ideky.Domain/Entity/GameResult.cs
+++ Api/Ideky/Ideky.Domain/Entity/GameResult.cs
@@ -8,16 +8,17 @@ namespace Ideky.Domain.Entity
         public int Id { get; private set; }
         public User User { get; private set; }
         public DateTime GameDate { get; private set; }
+        public int Score { get; private set; }
         public List<string> Messages { get; private set; }
 
         protected GameResult() { }
 
-        public GameResult(User user)
+        public GameResult(User user, int score)
         {
             Id = 0;
             User = user;
             GameDate = DateTime.Now;
-            
+            Score = score;   
         }
 
         public bool Validate()
