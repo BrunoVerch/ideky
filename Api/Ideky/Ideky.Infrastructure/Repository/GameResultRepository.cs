23839e4cff74a70d2022924b85697a8fc17cc1a5
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-07 15:34:18 -0300

    feat(Repository): implementação dos repositórios para as classes existentes

---
+++ Api/Ideky/Ideky.Infrastructure/Repository/GameResultRepository.cs
@@ -0,0 +1,42 @@
+﻿using Ideky.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ideky.Infrastructure.Repository
+{
+    public class GameResultRepository : IDisposable
+    {
+        private Context context;
+
+        public List<GameResult> GetList()
+        {
+            return context.GameResults.ToList();
+        }
+
+        public List<GameResult> GetListOrderByScore()
+        {
+            return context.GameResults.OrderBy(gameResult => gameResult.Score).ToList();
+        }
+
+        public GameResult GetById(int id)
+        {
+            return context.GameResults.FirstOrDefault(gameResult => gameResult.Id == id);
+        }
+
+        public List<GameResult> GetByUserId(int userFacebookId)
+        {
+            return context.GameResults.Where(gameResult => gameResult.User.FacebookId == userFacebookId).ToList();
+        }
+
+        public List<GameResult> GetByDate(DateTime gameDate)
+        {
+            return context.GameResults.Where(gameResult => gameResult.GameDate == gameDate).ToList();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ Api/Ideky/Ideky.Infrastructure/Repository/LevelRepository.cs
@@ -0,0 +1,34 @@
+﻿using Ideky.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ideky.Infrastructure.Repository
+{
+    public class LevelRepository : IDisposable
+    {
+        private Context context;
+
+        public Level GetById(int id)
+        {
+            return context.Levels.FirstOrDefault(level => level.Id == id);
+        }
+
+        public Level GetByLevelNumber(int levelNumber)
+        {
+            return context.Levels.FirstOrDefault(level => level.LevelNumber == levelNumber);
+        }
+
+        public List<Level> GetList()
+        {
+            return context.Levels.ToList();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
