5f28b1b848cea36e6da47c6620facbb9fb4b67c3
Tais <tais.silva@cwi.com.br>
2017-07-07 16:11:16 -0300

    feat(home) Acrescentar html e css inicial da home.

--- Front/www/app/app-core/app.routes.js
+++ Front/www/app/app-core/app.routes.js
@@ -6,6 +6,12 @@ angular
                 controller: 'LoginController',
                 templateUrl: 'app/app-modules/login/login.html'
             })
+            .when('/home', {
+                controller: 'SrtartController',
+                templateUrl: 'app/app-modules/home/home.html'
+            })
+
+
             .otherwise({
                 redirectTo: '/login'
             });
