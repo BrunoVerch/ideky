cfe0a9dfc25bc165648e1401a07fe4b5007768dc
Taís Jaques <Tas Jaques>
2017-07-09 13:26:38 -0300

    feat(administrative) Acrescentar pastas brutas, rotas e service de administrtive organizadas.

--- Front/www/app/app-core/app.routes.js
+++ Front/www/app/app-core/app.routes.js
@@ -14,6 +14,22 @@ angular
                 controller: 'GameController',
                 templateUrl: 'app/app-modules/game/game.html'
             })
+            .when('/loginadm', {
+                controller: 'LoginadmController',
+                templateUrl: 'app/app-modules/administrative/loginadm/loginadm.html'
+            })
+            .when('/menuadm', {
+                controller: 'MenuadmController',
+                templateUrl: 'app/app-modules/administrative/menuadm/menuadm.html'
+            })
+            .when('/lifesadm', {
+                controller: 'LifesadmController',
+                templateUrl: 'app/app-modules/administrative/lifesadm/lifesadm.html'
+            })
+            .when('/levelsadm', {
+                controller: 'LevelsadmController',
+                templateUrl: 'app/app-modules/administrative/levelsadm/levelsadm.html'
+            })
 
 
             .otherwise({
---
+++ Front/www/app/app-modules/administrative/menuadm/menuadm.controller.js
@@ -0,0 +1,24 @@
+angular
+	.module('app.core')
+	.controller('MenuadmController', function ($scope, AdministrativeService, toastr) {
+		
+		// FacebookId e número de vidas
+		$scope.addLifes = userLifes => {
+			AdministrativeService.addLifes(userLifes)
+				.then(response)
+				.catch(error => console.log(error))
+		}
+
+		$scope.editLevel = level => {
+			AdministrativeService.editLevel(level)
+				.then(response)
+				.catch(error => console.log(error));
+		}
+
+		$scope.resetRanking = () => {
+			AdministrativeService.resetRanking()
+				.then(response)
+				.catch(error => console.log(error));
+		}
+
+});
