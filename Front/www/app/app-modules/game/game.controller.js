83d88d45b9fd028753430f8c65d1391ebbc268b1
JabelFontoura <jabel_fontoura@hotmail.com>
2017-07-09 19:29:24 -0300

    feat(Amigos): buscar, validar e embaralhar amigos no game

--- Front/www/app/app-modules/game/game.controller.js
+++ Front/www/app/app-modules/game/game.controller.js
@@ -1,37 +1,54 @@
 angular
 	.module('app.core')
-	.controller('GameController', function ($scope, $window, GameService) {
-		
+	.controller('GameController', function ($rootScope, $scope, $window, $q, GameService, toastr) {
+			
+		$scope.start = () => {
+			loadFriends()
+				.then(response => {
+					if(response.data.length < 150) {
+						toastr.error('Você precisa de pelo menos 150 amigos para jogar.');
+						return;
+					}
 
-		setTimeout(function() {
-			init();
-			setTimeout(function() {
-				$scope.friends = shuffle($scope.friends);
-			}, 1000);
-		}, 5000);
+					$scope.friends = shuffle(response.data);
+					console.log($scope.friends);
 
-	function  init() {
-		//Carrega todos os amigos 
-		GameService.getFriends('friends', response => {
-			$scope.friends = response.data;
-			GameService.getFriends('invitable_friends', 
-				res => $scope.friends = $scope.friends.concat(res.data));
-		});
-  }
-
-	function shuffle(array) {
-		var currentIndex = array.length, temporaryValue, randomIndex;
-
-		while (0 !== currentIndex) {
-
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex -= 1;
-
-			temporaryValue = array[currentIndex];
-			array[currentIndex] = array[randomIndex];
-			array[randomIndex] = temporaryValue;
+				});
 		}
 
-		return array;
-	}
-});
+		function loadFriends() {
+			const deffered = $q.defer();
+			let friends;
+
+			$rootScope.sdkLoad
+				.then(response => {
+					GameService.getFriends('friends', response => {
+						friends = response.data;
+
+						GameService.getFriends('invitable_friends', 
+							res => { 
+								friends = friends.concat(res.data);
+								deffered.resolve({ data: friends });
+							});
+					});
+				});
+
+				return deffered.promise;
+		}
+
+		function shuffle(array) {
+			var currentIndex = array.length, temporaryValue, randomIndex;
+
+			while (0 !== currentIndex) {
+
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex -= 1;
+
+				temporaryValue = array[currentIndex];
+				array[currentIndex] = array[randomIndex];
+				array[randomIndex] = temporaryValue;
+			}
+
+			return array;
+		}
+	});
