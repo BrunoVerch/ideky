42c66832c43feeb85c1a13259d238190d6fea808
JabelFontoura <jabel_fontoura@hotmail.com>
2017-07-08 21:34:20 -0300

    feat(Game): adicionar shuffle para lista

--- Front/www/app/app-modules/game/game.controller.js
+++ Front/www/app/app-modules/game/game.controller.js
@@ -1,9 +1,13 @@
 angular
 	.module('app.core')
-	.controller('GameController', function ($rootScope, $scope, $window, GameService) {
+	.controller('GameController', function ($scope, $window, GameService) {
 		
+
 		setTimeout(function() {
 			init();
+			setTimeout(function() {
+				$scope.friends = shuffle($scope.friends);
+			}, 1000);
 		}, 5000);
 
 	function  init() {
@@ -12,7 +16,22 @@ angular
 			$scope.friends = response.data;
 			GameService.getFriends('invitable_friends', 
 				res => $scope.friends = $scope.friends.concat(res.data));
-
 		});
   }
+
+	function shuffle(array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		while (0 !== currentIndex) {
+
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+		return array;
+	}
 });
