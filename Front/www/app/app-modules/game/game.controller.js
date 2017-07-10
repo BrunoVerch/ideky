289822fb0ec659b16dbbabe0b00c43c353ff0616
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-10 18:50:47 -0300

    feat(Game/Tela): inicio da implementação da tela do jogo

--- Front/www/app/app-modules/game/game.controller.js
+++ Front/www/app/app-modules/game/game.controller.js
@@ -3,6 +3,13 @@ angular
 	.controller('GameController', function ($rootScope, $scope, $window, $location, $q, GameService, toastr) {
 			
 		init();
+		$scope.levels = [];
+		let levelNumber = 0;
+		$scope.level;
+		$scope.nextLevel = nextLevel;
+		$scope.drawFriends = [];
+		$scope.friends = [];
+		$scope.rightFriend = {};
 
 		function init() {
 			loadFriends()
@@ -12,9 +19,13 @@ angular
 						$location.path('/home');
 						return;
 					}
-
-					$scope.friends = shuffle(response.data);
-					console.log($scope.friends);
+					GameService.getLevels()
+					 	.then(levelResponse =>{
+					 		$scope.levels = levelResponse.data.data;							
+					 		$scope.friends = shuffle(response.data);
+					 		$scope.level = $scope.levels[levelNumber];
+							setDrawsFriends($scope.level.PictureAmount);
+					 	})
 				});
 		}
 
@@ -24,23 +35,44 @@ angular
 			
 			GameService.getFriends()
 				.then(response => deffered.resolve(response));
-				
+
 				return deffered.promise;
+		}
+
+		function nextLevel(){
+			$scope.friends = shuffle($scope.friends);
+			if(levelNumber<10){
+				levelNumber++;
+				$scope.level = $scope.levels[levelNumber];
+				getDrawsFriends($scope.level.PictureAmount);
+			}
+		}
+
+		function setDrawsFriends(pictureAmount){
+			for(let i=0; i<pictureAmount;i++){
+				let drawNumber = Math.floor(Math.random() * pictureAmount); 
+				$scope.drawFriends.push($scope.friends[i]);
+				console.log($scope.friends[i]);
+			}
+			let drawNumber = Math.floor(Math.random() * pictureAmount); 
+			$scope.rightFriend = $scope.drawFriends[drawNumber];
 		}
 
 		function shuffle(array) {
 			var currentIndex = array.length, temporaryValue, randomIndex;
-
 			while (0 !== currentIndex) {
-
 				randomIndex = Math.floor(Math.random() * currentIndex);
 				currentIndex -= 1;
-
-				temporaryValue = array[currentIndex];
-				array[currentIndex] = array[randomIndex];
-				array[randomIndex] = temporaryValue;
+				if(array[currentIndex].picture.data.is_silhouette === false){
+					temporaryValue = array[currentIndex];
+					array[currentIndex] = array[randomIndex];
+					array[randomIndex] = temporaryValue;
+				}		
 			}
-
 			return array;
+		}
+
+		function getLevels(){
+			
 		}
 	});
