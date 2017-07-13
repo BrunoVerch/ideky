61ce4bcdb985fb03bfbd1b055eb35fa037d91c36
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-13 19:57:58 -0300

    feat(life): implementação de sistema de vida ao jogo

--- Front/app/app-modules/home/home.controller.js
+++ Front/app/app-modules/home/home.controller.js
@@ -22,17 +22,12 @@ angular
 		$scope.start = () => {
 			$scope.friends = $localStorage.FriendsData;			 
 			if(typeof $scope.friends === 'undefined' || $scope.friends === null){
-				turnOnLoadingScreen();
-				startTextAnimation(500);
 				$timeout($scope.start,500);
-				$timeout(logout,5000);
 			}else if($scope.friends.length < 150){
 				toastr.error('Você precisa de pelo menos 150 amigos para jogar.');
 				$location.path('/home');
 				return;
 		 	}else{
-				stopTextAnimation();
-				turnOffLoadingScreen();
 				$location.path('/game');
 			}
 		}
@@ -50,38 +45,8 @@ angular
 		}
 
 		function updatePicture(user) {
-			user.Picture = user.picture.data.url;
-			user.FacebookId = user.id;
-
 			HomeService.updatePicture(user)
 						.then(response => console.log(response))
 						.catch(error => console.log(error));	
 		}
-
-		function turnOnLoadingScreen(){
-			$scope.loadingClass = 'loadingScreenOn';
-		}
-
-		function turnOffLoadingScreen(){
-			$scope.loadingClass = 'loadingScreenOff';
-		}
-
-		function startTextAnimation(delay){
-			textAnimationClasses = ['loadingTextStep1','loadingTextStep2','loadingTextStep3','loadingTextStep4'];
-			textAnimationCounter = 0;
-			textAnimationInterval = $interval(loadingTextAnimation,delay);
-		}
-		
-		function loadingTextAnimation(){
-			if(textAnimationCounter < textAnimationClasses.length){
-				$scope.loadingText = textAnimationClasses[textAnimationCounter];
-				textAnimationCounter++;
-			}else{
-				textAnimationCounter = 0;
-			}
-		}
-		function stopTextAnimation(){
-			$interval.cancel(textAnimationInterval);
-		}
-
 	});
