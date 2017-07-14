cd7f3d75499bdab0183face6e36fa1f2b89dae24
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-14 00:53:54 -0300

    feat(FriendsRankingApi): implementação de lógica para gerar um ranking de x usuários

--- Api/Ideky/Ideky.Api/Models/FriendsModel.cs
+++ Api/Ideky/Ideky.Api/Models/FriendsModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Ideky.Api.Models
+﻿namespace Ideky.Api.Models
 {
     public class FriendsModel
     {
+        public string name { get; set; }
+        public long id { get; set; }
     }
 }
